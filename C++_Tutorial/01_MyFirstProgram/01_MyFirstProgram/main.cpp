78c6a09498027f6b725cf6b907bed346905b3509
FRAZ5094 <60670235+FRAZ5094@users.noreply.github.com>
2020-09-25 08:59:58 +0100

    Cpp tutorials initial commit

---
+++ C++_Tutorial/01_MyFirstProgram/01_MyFirstProgram/main.cpp
@@ -0,0 +1,53 @@
+//Comments
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	/*
+	int x = 0;
+	int cars = 14;
+	int debt = -1000;
+	float cash = 2.32;
+	double credit = 32.32;
+	char a = 'a';
+	const char* sandwich = "ham";
+	bool does_she_like_me = true;
+	auto do_i_have_pants_on = false;
+	auto brank_check = 200.00;
+	int over_9000 = INT_MAX;
+
+	cout << over_9000 << endl;
+	*/
+	/*
+	cout << sizeof(char) << endl;
+	cout << sizeof(int) << endl;
+	cout << sizeof(float) << endl;
+	cout << sizeof(double) << endl;
+	cout << sizeof(bool) << endl;
+
+	cout << " " << endl;
+	cout << sizeof(short int) << endl;
+	cout << sizeof(long int) << endl; //default for integer
+	cout << sizeof(long long int) << endl;
+
+	*/
+	/*
+	signed int x = -1;
+	unsigned int y = -1; //can only be positive
+	
+	cout << x << endl;
+	cout << y << endl;
+	*/
+
+	cout << INT_MIN << endl;
+	cout << INT_MAX << endl;
+	cout << LONG_MIN << endl;
+	cout << LONG_MAX << endl;
+	cout << SHRT_MIN << endl;
+	cout << SHRT_MAX << endl;
+	cout << 0 << endl;
+	cout << USHRT_MAX << endl;
+
+	return 0;
+}
